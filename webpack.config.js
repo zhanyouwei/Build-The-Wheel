a908b04587aa0d58298a7b4be966fb561d04cbf5
Youwei <mr.zhanyouwei@gmail.com>
2015-12-08 00:42:16 +0800

    抽取通用第三方包，独立打包成库文件

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * Copyright 2015 kaiheiwang Corporation. All rights reserved.
  * Created by Youwei on 15/12/3.
  */
-
+var webpack = require('webpack');
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 
@@ -11,14 +11,17 @@ var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
 module.exports = {
-  entry: APP_PATH,
-  //entry: {
-  //app: './app/main.js'
-  //},
+  //entry: APP_PATH,
+  entry: {
+    app: APP_PATH,
+    //添加要打包在vendors里面的库
+    vendors: ['react', 'events', 'object-assign', 'superagent', 'moment']
+  },
   //输出的文件名 合并以后的js会命名为bundle.js
   output: {
     path: BUILD_PATH,
-    filename: 'bundle.js'
+    //filename: 'bundle.js'
+    filename: '[name].[hash].js'
   },
   devServer: {
     historyApiFallback: true,
@@ -26,6 +29,7 @@ module.exports = {
     inline: true,
     progress: true
   },
+  devtool: 'eval-source-map',//启用source-map
   module: {
     loaders: [
       {
@@ -61,6 +65,13 @@ module.exports = {
   },
   //自动生成一个html文件
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+    //new webpack.optimize.CommonsChunkPlugin('common.[hash].js'),
+    //new webpack.optimize.UglifyJsPlugin({
+    //  compress: {
+    //    warnings: false
+    //  }
+    //}),
     new HtmlwebpackPlugin({
       title: 'React Application Demo',
       template: 'app/my-index.html',
