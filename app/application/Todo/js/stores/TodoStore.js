b5194f4ca925dbb414c2520b984142e4b72f0065
Youwei <mr.zhanyouwei@gmail.com>
2015-12-08 00:08:10 +0800

    添加服务器端数据存储，多终端同步可以实现

--- app/application/Todo/js/stores/TodoStore.js
+++ app/application/Todo/js/stores/TodoStore.js
@@ -14,6 +14,7 @@ var EventEmitter = require('events').EventEmitter;
 var TodoConstants = require('../constants/TodoConstants');
 var assign = require('object-assign');
 const moment = require('moment');
+const superagent = require('superagent');
 var localstorage = require('../core/localstorage.util');
 
 var CHANGE_EVENT = 'change';
@@ -23,20 +24,42 @@ var _todos = JSON.parse(localStorage.getItem('todos')) || {};
 /**
  * Create a TODO item.
  * @param  {string} text The content of the TODO
+ * @param callback
  */
-function create(text) {
+function create(text,callback) {
   // Hand waving here -- not showing how this interacts with XHR or persistent
   // server-side storage.
   // Using the current timestamp + random number in place of a real id.
-  var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
-  _todos[id] = {
-    id: id,
+  let todo = {
     complete: false,
     text: text,
-    createdAt: moment().unix(),
     completeAt: null
   };
-  localStorage.setItem('todos', JSON.stringify(_todos));
+  superagent
+    .post('https://api.leancloud.cn/1.1/classes/Todo')
+    .set({
+      'X-LC-Id': 'H7CTUT7nifyX9bC88HVfnuMM',
+      'X-LC-Key': '8kUbCGgOvML7xtFMLPOQgoQ6'
+    })
+    .accept('application/json')
+    .send(todo)
+    .end((err, res) => {
+      if (err) {
+        if (err.status === 404) {
+          console.log(err);
+        } else {
+          console.log(err.error);
+        }
+        callback(err, null);
+      } else {
+        _todos[res.body.objectId] = assign(todo, {
+          id: res.body.objectId
+        }, res.body);
+        localStorage.setItem('todos', JSON.stringify(_todos));
+        callback(null, res.body);
+      }
+    });
+
 }
 
 /**
@@ -132,27 +155,30 @@ AppDispatcher.register(function (action) {
     case TodoConstants.TODO_CREATE:
       text = action.text.trim();
       if (text !== '') {
-        create(text);
-        TodoStore.emitChange();
+        create(text, function (err, result) {
+          if(!err) {
+            TodoStore.emitChange();
+          }
+        });
       }
       break;
 
     case TodoConstants.TODO_TOGGLE_COMPLETE_ALL:
       if (TodoStore.areAllComplete()) {
-        updateAll({complete: false,completeAt:null});
+        updateAll({complete: false, completeAt: null});
       } else {
-        updateAll({complete: true,completeAt:moment().unix()});
+        updateAll({complete: true, completeAt: moment().unix()});
       }
       TodoStore.emitChange();
       break;
 
     case TodoConstants.TODO_UNDO_COMPLETE:
-      update(action.id, {complete: false,completeAt:null});
+      update(action.id, {complete: false, completeAt: null});
       TodoStore.emitChange();
       break;
 
     case TodoConstants.TODO_COMPLETE:
-      update(action.id, {complete: true,completeAt:moment().unix()});
+      update(action.id, {complete: true, completeAt: moment().unix()});
       TodoStore.emitChange();
       break;
 
